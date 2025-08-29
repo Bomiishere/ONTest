33dca7ea5cf44815a71ad0166be6a4cba7fb1403
bomi <bomiishere@gmail.com>
2025-08-29 12:17:23 +0800

    init project

---
+++ ONTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ONTest
+//
+//  Created by bomi on 2025/8/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ONTest/ONTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ONTestApp.swift
+//  ONTest
+//
+//  Created by bomi on 2025/8/29.
+//
+
+import SwiftUI
+
+@main
+struct ONTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
