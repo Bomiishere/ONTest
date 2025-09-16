e30e4a7cb50650a6297d8be4484ff0fa203fdbfa
bomi <bomiishere@gmail.com>
2025-09-16 11:02:21 +0800

    fix tests

--- ONTestTests/MatchListFeatureTests.swift
+++ ONTestTests/MatchListFeatureTests.swift
@@ -16,7 +16,7 @@ final class MatchListFeatureTests: XCTestCase {
     
     enum TestErr: Error { case sth }
     
-    func testSend_task_emptyData() async {
+    func testTask() async {
         
         let store = TestStore(
             initialState: MatchListFeature.State.init(),
@@ -28,30 +28,19 @@ final class MatchListFeatureTests: XCTestCase {
             state.errorMessage = nil
         }
         await store.receive { action in
-            if case ._fetchCache = action { return true }
+            if case ._fetchMatchList = action { return true }
             return false
         }
-        await store.receive({ action in
-            if case ._fetchAPI = action { return true }
-            return false
-        })
         await store.receive { action in
             if case ._startOddsStream = action { return true }
             return false
         }
-        await store.receive({ action in
-            if case ._apply = action { return true }
-            return false
-        }) { state in
-            state.rows = []
-            state.isLoading = false
-        }
         
-        await store.send(.stop)
+        await store.send(.onDisappear)
         await store.finish()
     }
     
-    func test_fetchCache() async {
+    func testFetchMatchList_rows() async {
         let exp_rows: [MatchListFeature.State.Row] = [
             .init(id: 1, teamA: "A", teamB: "D", time: "2025-07-06T12:00:00Z", teamAOdds: "1.90", teamBOdds: "2.04"),
             .init(id: 2, teamA: "B", teamB: "C", time: "2025-07-05T12:00:00Z", teamAOdds: "1.80", teamBOdds: "2.20"),
@@ -61,12 +50,21 @@ final class MatchListFeatureTests: XCTestCase {
             initialState: MatchListFeature.State.init(isLoading: true),
             reducer: { MatchListFeature() }
         ) { deps in
-            deps.matchListRepo.fetchCache = { exp_rows }
+            deps.matchListRepo.fetchUpdates = {
+                AsyncThrowingStream<MatchListRepository.DataType, Error> { continuation in
+                    continuation.yield(exp_rows)
+                    continuation.finish()
+                }
+            }
         }
 
-        await store.send(._fetchCache)
+        await store.send(._fetchMatchList)
         await store.receive({ action in
             if case ._apply = action { return true }
+            return false
+        })
+        await store.receive({ action in
+            if case ._applyDonePatch = action { return true }
             return false
         }) { state in
             state.rows = .init(uniqueElements: exp_rows)
@@ -75,40 +73,51 @@ final class MatchListFeatureTests: XCTestCase {
         await store.finish()
     }
     
-    func test_fetchAPI() async {
+    func testFetchMatchList_rows_empty() async {
         let exp_rows: [MatchListFeature.State.Row] = [
-            .init(id: 1, teamA: "A", teamB: "D", time: "2025-07-06T12:00:00Z", teamAOdds: "1.90", teamBOdds: "2.04"),
-            .init(id: 2, teamA: "B", teamB: "C", time: "2025-07-05T12:00:00Z", teamAOdds: "1.80", teamBOdds: "2.20"),
         ]
         
         let store = TestStore(
             initialState: MatchListFeature.State.init(isLoading: true),
             reducer: { MatchListFeature() }
         ) { deps in
-            deps.matchListRepo.fetchAPI = { exp_rows }
+            deps.matchListRepo.fetchUpdates = {
+                AsyncThrowingStream<MatchListRepository.DataType, Error> { continuation in
+                    continuation.yield(exp_rows)
+                    continuation.finish()
+                }
+            }
         }
 
-        await store.send(._fetchAPI)
+        await store.send(._fetchMatchList)
         await store.receive({ action in
             if case ._apply = action { return true }
             return false
-        }) { status in
-            status.rows = .init(uniqueElements: exp_rows)
-            status.isLoading = false
+        })
+        await store.receive({ action in
+            if case ._applyDonePatch = action { return true }
+            return false
+        }) { state in
+            state.rows = .init(uniqueElements: exp_rows)
+            state.isLoading = false
         }
         await store.finish()
     }
-  
-    func test_fetchAPI_failure() async {
+
+    func testFetchMatchList_failure() async {
         
         let store = TestStore(
             initialState: MatchListFeature.State.init(),
             reducer: { MatchListFeature()}
         )  { deps in
-            deps.matchListRepo.fetchAPI = { throw TestErr.sth }
+            deps.matchListRepo.fetchUpdates = {
+                AsyncThrowingStream<MatchListRepository.DataType, Error> { continuation in
+                    continuation.finish(throwing: TestErr.sth)
+                }
+            }
         }
-
-        await store.send(._fetchAPI)
+        
+        await store.send(._fetchMatchList)
         await store.receive({ action in
             if case let ._failed(msg) = action {
                 XCTAssertTrue(msg.contains("sth"))
@@ -122,30 +131,14 @@ final class MatchListFeatureTests: XCTestCase {
         await store.finish()
     }
 
-    func test_oddsStream_stop() async {
-        let terminated = expectation(description: "stream terminated")
-        let stream = AsyncStream<OddsUpdate> { continuation in
-            continuation.onTermination = { _ in
-                terminated.fulfill()
-            }
-        }
-        
-        let store = TestStore(
-            initialState: MatchListFeature.State.init(),
-            reducer: { MatchListFeature()}
-        )  { deps in
-            deps.ws.oddsUpdate = { stream }
-        }
-        await store.send(._startOddsStream)
-        await store.send(.stop)
-        await fulfillment(of: [terminated], timeout: 1.0)
-        
-        await store.finish()
-    }
- 
-    func test_oddsStream_start_takeLatestOddsUpdateInOneSecond() async {
+    func testOddsStream_start_takeLatestOddsUpdateInOneSecond() async {
         
         let scheduler = DispatchQueue.test
+        let exp_updates: [OddsUpdate] = [
+            .init(matchID: 1, teamAOdds: 1.80, teamBOdds: 2.10),
+            .init(matchID: 1, teamAOdds: 1.85, teamBOdds: 2.05),
+            .init(matchID: 1, teamAOdds: 1.90, teamBOdds: 2.00),
+        ]
         
         let store = TestStore(
             initialState: MatchListFeature.State.init(),
@@ -154,23 +147,25 @@ final class MatchListFeatureTests: XCTestCase {
             deps.mainQueue = scheduler.eraseToAnyScheduler()
             deps.ws.oddsUpdate = {
                 AsyncStream { continuation in
-                    continuation.yield(.init(matchID: 1, teamAOdds: 1.80, teamBOdds: 2.10))
-                    continuation.yield(.init(matchID: 1, teamAOdds: 1.85, teamBOdds: 2.05))
-                    continuation.yield(.init(matchID: 1, teamAOdds: 1.90, teamBOdds: 2.00))
+                    for update in exp_updates {
+                        continuation.yield(update)
+                    }
                     continuation.finish()
                 }
             }
         }
         
         // apply rows
-        await store.send(._apply([
+        let exp_rows: [MatchListFeature.State.Row] = [
             .init(id: 1, teamA: "A", teamB: "B", time: "2025-07-06T12:00:00Z", teamAOdds: "-", teamBOdds: "-"),
             .init(id: 2, teamA: "C", teamB: "D", time: "2025-07-05T12:00:00Z", teamAOdds: "-", teamBOdds: "-"),
-        ])) { state in
-            state.rows = [
-                .init(id: 1, teamA: "A", teamB: "B", time: "2025-07-06T12:00:00Z", teamAOdds: "-", teamBOdds: "-"),
-                .init(id: 2, teamA: "C", teamB: "D", time: "2025-07-05T12:00:00Z", teamAOdds: "-", teamBOdds: "-"),
-            ]
+        ]
+        await store.send(._apply(.init(uniqueElements: exp_rows)))
+        await store.receive({ action in
+            if case ._applyDonePatch = action { return true }
+            return false
+        }) { state in
+            state.rows = .init(uniqueElements: exp_rows)
             state.isLoading = false
         }
 
@@ -183,16 +178,16 @@ final class MatchListFeatureTests: XCTestCase {
         // update 3 筆
         for _ in 0..<3 {
             await store.receive({ action in
-                if case ._throttleStreamUpdate = action { return true }
+                if case ._throttleOddsUpdate = action { return true }
                 return false
             })
         }
         // verify last OddsUpdate in one sec
         await store.receive({ action in
-            if case let ._updateOdds(update) = action {
-                XCTAssertEqual(update.matchID, 1)
-                XCTAssertEqual(update.teamAOdds, 1.90, accuracy: 0.0001)
-                XCTAssertEqual(update.teamBOdds, 2.00, accuracy: 0.0001)
+            if case let ._updateOdds(update) = action, let last_update = exp_updates.last {
+                XCTAssertEqual(last_update.matchID, 1)
+                XCTAssertEqual(last_update.teamAOdds, 1.90, accuracy: 0.0001)
+                XCTAssertEqual(last_update.teamBOdds, 2.00, accuracy: 0.0001)
                 return true
             }
             return false
@@ -202,14 +197,10 @@ final class MatchListFeatureTests: XCTestCase {
                 state.rows[idx].teamBOdds = "2.00"
             }
         }
-        await store.receive { action in
-            if case ._updateOddsRepo = action { return true }
-            return false
-        }
         await store.finish()
     }
     
-    func test_oddsStream_fail() async {
+    func testOddsStream_fail() async {
         let scheduler = DispatchQueue.test
         
         let store = TestStore(
@@ -230,6 +221,28 @@ final class MatchListFeatureTests: XCTestCase {
         }) { state in
             state.errorMessage = String(describing: TestErr.sth)
         }
+        await store.finish()
+    }
+    
+    
+    func testOddsStream_stop() async {
+        let terminated = expectation(description: "stream terminated")
+        let stream = AsyncStream<OddsUpdate> { continuation in
+            continuation.onTermination = { _ in
+                terminated.fulfill()
+            }
+        }
+        
+        let store = TestStore(
+            initialState: MatchListFeature.State.init(),
+            reducer: { MatchListFeature()}
+        )  { deps in
+            deps.ws.oddsUpdate = { stream }
+        }
+        await store.send(._startOddsStream)
+        await store.send(.onDisappear)
+        await fulfillment(of: [terminated], timeout: 1.0)
+        
         await store.finish()
     }
 }
